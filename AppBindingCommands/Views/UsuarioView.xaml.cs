6a97cc8d886afea3ef9e0e39fd7819e931a5fa69
Henrique Gonçalves 22 <henrique.gonc.gamer@gmail.com>
2025-03-09 11:23:35 -0300

    Respostas

--- AppBindingCommands/Views/UsuarioView.xaml.cs
+++ AppBindingCommands/Views/UsuarioView.xaml.cs
@@ -4,9 +4,13 @@ namespace AppBindingCommands.Views;
 
 public partial class UsuarioView : ContentPage
 {
-	public UsuarioView()
-	{
-		InitializeComponent();
-		BindingContext = new UsuarioViewModel();
-	}
+    private UsuarioViewModel viewModel;
+
+    public UsuarioView()
+    {
+        InitializeComponent();
+        viewModel = new UsuarioViewModel();
+        BindingContext = viewModel;
+    }
+
 }
